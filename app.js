46de2943348d8a9c1416db3a777b95ceb50726cc
Ahmed Mostafa <ahmedabdo98648@gmail.com>
2023-11-27 12:34:37 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const connectDB = require('./db/connect')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+
+
+app.use('/api/v1/tasks',tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+
+
+const port = process.env.PORT || 3000
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`server is listening on port ${port}...`))
+
+    }
+    catch (err){
+        console.log(err);
+
+    }
+}
+
+start()
